1de41599426d84dd207991a292315cc203a6fb9a
Guilherme <guilhermeshadow91@gmail.com>
2023-02-07 14:39:15 -0300

    refactor: removing try and catch statement

--- src/controllers/ClassesController.controller.js
+++ src/controllers/ClassesController.controller.js
@@ -1,60 +1,48 @@
 const database = require('../models')
 
 class ClassesController {
-  static async getAllClasses(req, res){
-    try {
-      const allClasses = await database.Classes.findAll()
-      return res.status(200).json(allClasses)  
-    } catch (error) {
-      return res.status(500).json(error.message)
-    }
+  static async getAllClasses(req, res) {
+
+    const allClasses = await database.Classes.findAll()
+    return res.status(200).json(allClasses)
+
   }
 
   static async getOneClass(req, res) {
     const { id } = req.params
-    try {
-      const oneClass = await database.Classes.findOne( { 
-        where: { 
-          id: Number(id) 
-        }
-      })
-      return res.status(200).json(oneClass)
-    } catch (error) {
-      return res.status(500).json(error.message)
-    }
+    const oneClass = await database.Classes.findOne({
+      where: {
+        id: Number(id)
+      }
+    })
+    return res.status(200).json(oneClass)
+
   }
 
   static async createClass(req, res) {
     const newClass = req.body
-    try {
-      const newClassCreated = await database.Classes.create(newClass)
-      return res.status(200).json(newClassCreated)
-    } catch (error) {
-      return res.status(500).json(error.message)
-    }
+
+    const newClassCreated = await database.Classes.create(newClass)
+    return res.status(200).json(newClassCreated)
+
   }
 
   static async updateClass(req, res) {
     const { id } = req.params
     const newInfos = req.body
-    try {
-      await database.Classes.update(newInfos, { where: { id: Number(id) }})
-      const levelUpdated = await database.Classes.findOne( { where: { id: Number(id) }})
-      return res.status(200).json(levelUpdated)
-    } catch (error) {
-      return res.status(500).json(error.message)
-    }
+
+    await database.Classes.update(newInfos, { where: { id: Number(id) } })
+    const levelUpdated = await database.Classes.findOne({ where: { id: Number(id) } })
+    return res.status(200).json(levelUpdated)
+
   }
 
   static async deleteClass(req, res) {
     const { id } = req.params
-    try {
-      await database.Classes.destroy({ where: { id: Number(id) }})
-      return res.status(200).json({ message: `ID ${id} deleted with sucess` })
 
-    } catch (error) {
-      return res.status(500).json(error.message)
-    }
+    await database.Classes.destroy({ where: { id: Number(id) } })
+    return res.status(200).json({ message: `ID ${id} deleted with sucess` })
+
   }
 
 }

1cf6917f41ddf5c0fd0d73269368a4958e894d29
Guilherme <guilhermeshadow91@gmail.com>
2023-02-07 14:30:35 -0300

    refactor: changing the functionsname to class

--- src/controllers/ClassesController.controller.js
+++ src/controllers/ClassesController.controller.js
@@ -27,29 +27,29 @@ class ClassesController {
   static async createClass(req, res) {
     const newClass = req.body
     try {
-      const newClassCreated = await database.Class.create(newClass)
+      const newClassCreated = await database.Classes.create(newClass)
       return res.status(200).json(newClassCreated)
     } catch (error) {
       return res.status(500).json(error.message)
     }
   }
 
-  static async updateLevel(req, res) {
+  static async updateClass(req, res) {
     const { id } = req.params
     const newInfos = req.body
     try {
-      await database.Levels.update(newInfos, { where: { id: Number(id) }})
-      const levelUpdated = await database.Levels.findOne( { where: { id: Number(id) }})
+      await database.Classes.update(newInfos, { where: { id: Number(id) }})
+      const levelUpdated = await database.Classes.findOne( { where: { id: Number(id) }})
       return res.status(200).json(levelUpdated)
     } catch (error) {
       return res.status(500).json(error.message)
     }
   }
 
-  static async deleteLevel(req, res) {
+  static async deleteClass(req, res) {
     const { id } = req.params
     try {
-      await database.Levels.destroy({ where: { id: Number(id) }})
+      await database.Classes.destroy({ where: { id: Number(id) }})
       return res.status(200).json({ message: `ID ${id} deleted with sucess` })
 
     } catch (error) {

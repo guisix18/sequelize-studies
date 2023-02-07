218b8b5fc6a19a85cfd9818babf159c30085d00a
Guilherme <guilhermeshadow91@gmail.com>
2023-02-07 14:30:59 -0300

    feat: port is now 3001 and using class routes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const peoplesRoutes = require("./routes/people.route");
 const levelsRoutes = require("./routes/level.route");
+const classRoutes = require("./routes/classes.route");
 
 let cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/peoples', peoplesRoutes)
-app.use('/levels', levelsRoutes)
+app.use('/peoples', peoplesRoutes);
+app.use('/levels', levelsRoutes);
+app.use('/classes', classRoutes);
 
-const port = 3000;
-
-app.get("/hello", (req, res) => res.status(200).send({message: "Hello World!"}))
+const port = 3001;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`);
 })
 
 
-module.exports = {app}
+module.exports = {app};

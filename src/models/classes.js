32859b6861295357faa9c4c07f7b3e34cd323fef
Guilherme <guilhermeshadow91@gmail.com>
2023-02-07 14:31:38 -0300

    refactor: chaging field to the right name

--- src/models/classes.js
+++ src/models/classes.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Classes.associate = function(models) {
     Classes.hasMany(models.Registration, {foreignKey: 'class_id'});
-    Classes.belongsTo(models.Peoples, {foreignKey: 'subjetc_id'});
+    Classes.belongsTo(models.Peoples, {foreignKey: 'subject_id'});
     Classes.belongsTo(models.Levels, {foreignKey: 'level_id'});
   };
   return Classes;
